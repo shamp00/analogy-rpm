f56fff70487d5da56199c514526aaf627f1d02f1
Robert Anderson <github@nosredna.com>
2019-07-04 13:14:32 +0100

    No improvement with eta=0.2

--- CHL.py
+++ CHL.py
@@ -109,8 +109,8 @@ class Config:
     min_error: float = 0.001
     max_epochs: int = 40000
     max_activation_cycles: int = 100 # The maximum number of times the activation is propagated. 
-    max_activation_cycles_fully_unclamped = 100
-    eta: float = 0.05
+    max_activation_cycles_fully_unclamped = 1
+    eta: float = 0.2
     noise: float = 0.
     adaptive_bias: bool = True
     early_hidden_layer_update: bool = True # False is good.
@@ -292,7 +292,7 @@ class Network:
         self.set_inputs(p)
         clamps = ['input', 'transformation']
         if is_primed:
-            if self.config.strict_leech:
+            if False and self.config.strict_leech:
                 clamps = ['input']
                 # Not sure about this. Why not leave the primed transformation input?
                 self.reset_transformation_to_rest()
