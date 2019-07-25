9399b629ddf1e484f5f792e39765e14065eef752
Robert Anderson <github@nosredna.com>
2019-07-25 08:40:06 +0100

    Surprisingly poor performance with eta 0.1 and recent bugfix for unclamped portion of unlearn.

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     max_epochs: int = 40000
     max_activation_cycles: int = 100 # The maximum number of times the activation is propagated. 
     max_activation_cycles_fully_unclamped: int = 100
-    eta: float = 0.05
+    eta: float = 0.1
     sigmoid_smoothing: float = 0.1
     noise: float = 0.
     adaptive_bias: bool = True
