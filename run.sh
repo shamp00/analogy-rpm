654cbaac97e2ebb8607a7e4e5e71493793127675
Robert Anderson <github@nosredna.com>
2019-07-27 01:32:35 +0100

    Added run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt-get install -y --no-install-recommends \
+    libcairo2-dev
+
+pip install pycairo
+pip install dataclasses
+pip install numba
+pip install colorama
+pip install git+https://github.com/shamp00/pyRavenMatrices#egg=pyRavenMatrices
+
+python RPM.py 
