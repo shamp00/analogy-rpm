f31b266c51d2b5e87a3270d59cc6470c50d32cd7
Robert Anderson <github@nosredna.com>
2019-07-29 21:38:28 +0100

    Experiment 4 settings.

--- config.py
+++ config.py
@@ -2,20 +2,20 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    experiment_name = 'experiment_3'
-    experiment_description = """This is experiment 3."""
+    experiment_name = 'experiment_4'
+    experiment_description = """This is experiment 4."""
 
     # Hyperparameters
     learning_strategy: str = 'async'
     n_inputs: int = 11
     n_transformation: int = 4
-    n_hidden: int = 30
+    n_hidden: int = 14
     n_outputs: int = 11 
     min_error: float = 0.001
     max_epochs: int = 40000
     max_activation_cycles: int = 100 # The maximum number of times the activation is propagated. 
     max_activation_cycles_fully_unclamped: int = 100
-    eta: float = 0.02
+    eta: float = 0.1
     sigmoid_smoothing: float = 0.1
     noise: float = 0.
     adaptive_bias: bool = True
