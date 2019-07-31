e455c12999ff02a9564e61213789581693fa1f58
Robert Anderson <github@nosredna.com>
2019-07-31 08:43:33 +0100

    Experiment 1: Run 7. Eta is 0.05.

--- config.py
+++ config.py
@@ -2,21 +2,21 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    experiment_name = 'experiment 2'
-    experiment_description = """This is experiment 1. Run 7. No sigmoid smoothing. Lower eta."""
+    experiment_name = 'experiment 7'
+    experiment_description = """This is experiment 1. Run 7. eta 0.05."""
 
     # Hyperparameters
     network_type: str = ''
     learning_strategy: str = 'async'
     n_inputs: int = 11
     n_transformation: int = 4
-    n_hidden: int = 1
+    n_hidden: int = 30
     n_outputs: int = 11 
     min_error: float = 0.001
     max_epochs: int = 40000
     max_activation_cycles: int = 100 # The maximum number of times the activation is propagated. 
     max_activation_cycles_fully_unclamped: int = 100
-    eta: float = 0.00005
+    eta: float = 0.05
     sigmoid_smoothing: float = 1
     noise: float = 0.
     adaptive_bias: bool = True
