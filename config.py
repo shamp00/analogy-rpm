e57e14ce61f45e234960e0002ffef4fbdc50aa1c
Robert Anderson <github@nosredna.com>
2019-07-31 20:46:36 +0100

    Experiment 1: Run 10. 30 hidden. Only 1 cycle unclamped.

--- config.py
+++ config.py
@@ -2,20 +2,20 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    experiment_name = 'experiment_1_9'
-    experiment_description = """This is experiment 1. Run 9. 50 Hidden units."""
+    experiment_name = 'experiment_1_10'
+    experiment_description = """This is experiment 1. Run 10. 30 Hidden units. Only 1 cycle unclamped."""
 
     # Hyperparameters
     network_type: str = ''
     learning_strategy: str = 'async'
     n_inputs: int = 11
     n_transformation: int = 4
-    n_hidden: int = 50
+    n_hidden: int = 30
     n_outputs: int = 11 
     min_error: float = 0.001
     max_epochs: int = 40000
     max_activation_cycles: int = 100 # The maximum number of times the activation is propagated. 
-    max_activation_cycles_fully_unclamped: int = 100
+    max_activation_cycles_fully_unclamped: int = 1
     eta: float = 0.05
     sigmoid_smoothing: float = 0.1
     noise: float = 0.
