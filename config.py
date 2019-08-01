0d0d412adb00bb22a6f96e567652a9bf1315c1e5
Robert Anderson <github@nosredna.com>
2019-08-01 23:06:39 +0100

    Experiment 1: Run 12. 50 hidden units. No reset to rest after priming.

--- config.py
+++ config.py
@@ -2,21 +2,21 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    experiment_name = 'experiment_2_13'
-    experiment_description = """This is experiment 2. Run 13. 30 Hidden units. Sigmoid smoothing 0.1. Eta 0.0001."""
+    experiment_name = 'experiment_1_12'
+    experiment_description = """This is experiment 1. Run 12. 50 Hidden units. No reset to rest after priming."""
 
     # Hyperparameters
     network_type: str = ''
     learning_strategy: str = 'async'
     n_inputs: int = 11
     n_transformation: int = 4
-    n_hidden: int = 30
+    n_hidden: int = 50
     n_outputs: int = 11 
     min_error: float = 0.001
     max_epochs: int = 40000
     max_activation_cycles: int = 100 # The maximum number of times the activation is propagated. 
     max_activation_cycles_fully_unclamped: int = 100
-    eta: float = 0.0001
+    eta: float = 0.05
     sigmoid_smoothing: float = 0.1
     noise: float = 0.
     adaptive_bias: bool = True
