3f194bc31227f917b3cb0524a9034b8072b36fb2
Robert Anderson <github@nosredna.com>
2019-08-01 22:08:44 +0100

    Fix for agg with Seaborn.

--- RPM.py
+++ RPM.py
@@ -23,13 +23,14 @@ from methods import mean_squared_error
 from printing import (Lexicon, generate_rpm_2_by_2_matrix,
                       generate_rpm_2_by_3_matrix, generate_rpm_3_by_3_matrix,
                       is_running_from_ipython, is_paperspace, target, test_matrix)
-import seaborn as sns
-sns.set()
 
 if not is_running_from_ipython():
     if "Darwin" not in platform.platform():
         # Must be run before importing matplotlib.pyplot
         matplotlib.use('agg')
+
+import seaborn as sns
+sns.set()
 
 import matplotlib.pyplot as plt
 
