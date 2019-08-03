e17010b5d64434b702ecc70a14b79d0a36bbf08f
Robert Anderson <github@nosredna.com>
2019-08-03 11:13:15 +0100

    Experiment 2: Run 16. Input clamped followed by unclamped.

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    experiment_name = 'experiment_2_15'
-    experiment_description = """This is experiment 2. Run 15. 30 Hidden units. Improved analogy completion for 2x3 and 3x3. Also, testing different unlearn scenarios. Here we are using input clamped."""
+    experiment_name = 'experiment_2_16'
+    experiment_description = """This is experiment 2. Run 16. 30 Hidden units. Improved analogy completion for 2x3 and 3x3. Also, testing different unlearn scenarios. Here we are using input clamped followed by unclamped."""
 
     # Hyperparameters
     network_type: str = ''
@@ -15,7 +15,7 @@ class Config:
     min_error: float = 0.001
     max_epochs: int = 40000
     max_activation_cycles: int = 100 # The maximum number of times the activation is propagated. 
-    max_activation_cycles_fully_unclamped: int = 0
+    max_activation_cycles_fully_unclamped: int = 100
     eta: float = 0.0002
     sigmoid_smoothing: float = 0.1
     noise: float = 0.
