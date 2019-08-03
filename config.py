006d714b7a382e352336d407e2206f5c2f71e7fb
Robert Anderson <github@nosredna.com>
2019-08-03 22:43:36 +0100

    Experiment 1: Run 17. Comparing with fewer hidden units.

--- config.py
+++ config.py
@@ -2,21 +2,21 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    experiment_name = 'experiment_2_19'
-    experiment_description = """This is experiment 2. Run 19. 1 hidden units. Comparing with 2_18 with 1 hidden unit."""
+    experiment_name = 'experiment_1_17'
+    experiment_description = """This is experiment 1. Run 17. 15 hidden units. Comparing with 1_16 which had 50 hidden units."""
 
     # Hyperparameters
     network_type: str = ''
     learning_strategy: str = 'async'
     n_inputs: int = 11
     n_transformation: int = 4
-    n_hidden: int = 1
+    n_hidden: int = 15
     n_outputs: int = 11 
     min_error: float = 0.001
     max_epochs: int = 40000
     max_activation_cycles: int = 100 # The maximum number of times the activation is propagated. 
-    max_activation_cycles_fully_unclamped: int = 0
-    eta: float = 0.0003
+    max_activation_cycles_fully_unclamped: int = 100
+    eta: float = 0.05
     sigmoid_smoothing: float = 0.1
     noise: float = 0.
     adaptive_bias: bool = True
@@ -24,3 +24,29 @@ class Config:
     clamp_input_only_during_priming: bool = False
     learn_patterns_explicitly: bool = True
     learn_transformations_explicitly: bool = False
+
+
+# @dataclass
+# class Config:
+#     experiment_name = 'experiment_2_19'
+#     experiment_description = """This is experiment 2. Run 19. 1 hidden units. Comparing with 2_18 with 1 hidden unit."""
+
+#     # Hyperparameters
+#     network_type: str = ''
+#     learning_strategy: str = 'async'
+#     n_inputs: int = 11
+#     n_transformation: int = 4
+#     n_hidden: int = 1
+#     n_outputs: int = 11 
+#     min_error: float = 0.001
+#     max_epochs: int = 40000
+#     max_activation_cycles: int = 100 # The maximum number of times the activation is propagated. 
+#     max_activation_cycles_fully_unclamped: int = 0
+#     eta: float = 0.0003
+#     sigmoid_smoothing: float = 0.1
+#     noise: float = 0.
+#     adaptive_bias: bool = True
+#     strict_leech: bool = True
+#     clamp_input_only_during_priming: bool = False
+#     learn_patterns_explicitly: bool = True
+#     learn_transformations_explicitly: bool = False
