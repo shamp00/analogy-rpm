d94a29f4b9e2e9cc96b66a959e97a912b72a101e
Robert Anderson <github@nosredna.com>
2019-08-03 11:05:54 +0100

    Forgot config.

--- config.py
+++ config.py
@@ -2,21 +2,21 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    experiment_name = 'experiment_1_15'
-    experiment_description = """This is experiment 1. Run 15. 50 Hidden units. Same as before. Quick check that nothing is no worse since recent fixes to analogy completion."""
+    experiment_name = 'experiment_2_15'
+    experiment_description = """This is experiment 2. Run 15. 30 Hidden units. Improved analogy completion for 2x3 and 3x3. Also, testing different unlearn scenarios. Here we are using input clamped."""
 
     # Hyperparameters
     network_type: str = ''
     learning_strategy: str = 'async'
     n_inputs: int = 11
     n_transformation: int = 4
-    n_hidden: int = 50
+    n_hidden: int = 30
     n_outputs: int = 11 
     min_error: float = 0.001
     max_epochs: int = 40000
     max_activation_cycles: int = 100 # The maximum number of times the activation is propagated. 
-    max_activation_cycles_fully_unclamped: int = 100
-    eta: float = 0.05
+    max_activation_cycles_fully_unclamped: int = 0
+    eta: float = 0.0002
     sigmoid_smoothing: float = 0.1
     noise: float = 0.
     adaptive_bias: bool = True
