46e80436c99640f42412971ef5db3a44eb52bccf
Robert Anderson <github@nosredna.com>
2019-08-10 08:54:10 +0100

    Experiment 2. Run 26. Improved Smolensky propagation.

--- config.py
+++ config.py
@@ -1,38 +1,9 @@
 from dataclasses import dataclass
 
-@dataclass
-class Config:
-    experiment_name = 'experiment_1_21'
-    experiment_description = """This is experiment 1. Run 21. 14 hidden units. Smolensky."""
-
-    # Hyperparameters
-    network_type: str = ''
-    learning_strategy: str = 'async'
-    n_sample_size: int = 1000
-    n_inputs: int = 11
-    n_transformation: int = 4
-    n_hidden: int = 14
-    n_outputs: int = 11 
-    min_error: float = 0.001
-    max_epochs: int = 40000
-    max_activation_cycles: int = 100 # The maximum number of times the activation is propagated. 
-    max_activation_cycles_fully_unclamped: int = 100
-    eta: float = 0.05
-    sigmoid_smoothing: float = 0.1
-    noise: float = 0.
-    adaptive_bias: bool = True
-    strict_leech: bool = True
-    clamp_input_only_during_priming: bool = False
-    learn_patterns_explicitly: bool = True
-    learn_transformations_explicitly: bool = False
-    use_voting_for_3_by_3: bool = False
-    smolensky_propagation: bool = True
-
-
 # @dataclass
 # class Config:
-#     experiment_name = 'experiment_2_25'
-#     experiment_description = """This is experiment 2. Run 25. 14 hidden units. Test of new Smolensky propagation."""
+#     experiment_name = 'experiment_1_21'
+#     experiment_description = """This is experiment 1. Run 21. 14 hidden units. Smolensky."""
 
 #     # Hyperparameters
 #     network_type: str = ''
@@ -46,7 +17,7 @@ class Config:
 #     max_epochs: int = 40000
 #     max_activation_cycles: int = 100 # The maximum number of times the activation is propagated. 
 #     max_activation_cycles_fully_unclamped: int = 0
-#     eta: float = 0.0003
+#     eta: float = 0.05
 #     sigmoid_smoothing: float = 0.1
 #     noise: float = 0.
 #     adaptive_bias: bool = True
@@ -56,3 +27,32 @@ class Config:
 #     learn_transformations_explicitly: bool = False
 #     use_voting_for_3_by_3: bool = False
 #     smolensky_propagation: bool = True
+
+
+@dataclass
+class Config:
+    experiment_name = 'experiment_2_26'
+    experiment_description = """This is experiment 2. Run 26. 14 hidden units. Improved Smolensky propagation."""
+
+    # Hyperparameters
+    network_type: str = ''
+    learning_strategy: str = 'async'
+    n_sample_size: int = 1000
+    n_inputs: int = 11
+    n_transformation: int = 4
+    n_hidden: int = 14
+    n_outputs: int = 11 
+    min_error: float = 0.001
+    max_epochs: int = 40000
+    max_activation_cycles: int = 100 # The maximum number of times the activation is propagated. 
+    max_activation_cycles_fully_unclamped: int = 0
+    eta: float = 0.0003
+    sigmoid_smoothing: float = 0.1
+    noise: float = 0.
+    adaptive_bias: bool = True
+    strict_leech: bool = True
+    clamp_input_only_during_priming: bool = False
+    learn_patterns_explicitly: bool = True
+    learn_transformations_explicitly: bool = False
+    use_voting_for_3_by_3: bool = False
+    smolensky_propagation: bool = True
